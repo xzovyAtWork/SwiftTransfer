d502eb1bb7733d9647b1c815f78a2d79a4229ed1
Caleb Steinwandt <jsteinca@jci.com>
2026-08-18 07:47:53 -0600

    create ballard specific script

--- BallardSwiftTransfer.ps1
+++ BallardSwiftTransfer.ps1
@@ -9,38 +9,43 @@ function moveFiles {
     $JobNumber = $JobNumberInput.text
     $Prefix =  $JobNumber.ToString().substring(0, 2) + "00"
 
-    $RootFolder = "$Base\$Prefix"
+    $DestURL = "$Base\$Prefix"
 
     try {
-        $JobFolders = Get-ChildItem -Path $RootFolder -Filter "*$JobNumber*"
-        if ($JobFolders.Count -eq 0) {
+        $JobNumberFolder = Get-ChildItem -Path $RootFolder -Filter "*$JobNumber*"
+        if ($JobNumberFolder.Count -eq 0) {
             throw "No job folders found matching the pattern *$JobNumber*"
+        }elseif($JobNumberFolder.Count -gt 1){
+            #multiple selections found. get user input
+            $i = 0
+            Write-Host "=== SELECTION ===" -ForegroundColor Cyan
+            for ($i; $i -le $JobNumberFolder.Count - 1; $i++) {
+                $var = $JobNumberFolder[$i]
+                Write-Host "$i. $var"   
+            }
+            Write-Host "e. Exit"
+    
+            $choice = Read-Host "Please select an option"
+            $temp = ""
+            switch ($choice) {
+            "$_" {
+                $temp = $JobNumberFolder[$_]
+                $DestURL = "$DestUrl\$temp"
+            }
+            "e"{
+                Write-Host "Exiting..."
+                break;
+            }
+            Default { 
+                Write-Host "Invalid option, try again." -ForegroundColor Red; Start-Sleep -Seconds 1 }
+            }
         }
     } catch {
         Write-Host "Error: $_"
         return
     }
-
-    foreach ($job in $JobFolders) {
-        try {
-            $qualityPath = "$RootFolder\$job\2 Quality"
-
-            Write-Host "Checking path: $qualityPath"
-
-            if (Test-Path $qualityPath) {
-                if (Get-ChildItem -Path $qualityPath -Filter "*Units*") {
-                    $QualityRootFolder = $qualityPath
-                    Write-Host "Quality Root Folder found: $QualityRootFolder"
-                } else {
-                    Write-Host "No Units folder found in: $qualityPath"
-                }
-            } else {
-                Write-Host "Path does not exist: $qualityPath"
-            }
-        } catch {
-            Write-Host "Error accessing path: $_"
-        }
-    }
+    $QualityURL = Get-ChildItem -Path $DestURL -Filter "*Quality*"
+    $DestURL = "$DestURL\$QualityURL"
 
     switch ($UnitNumber) {
         {$_ -le 32} {
@@ -48,23 +53,23 @@ function moveFiles {
             break
         }
         {$_ -ge 33 -and $_ -le 64} {
-            $Colo = Get-ChildItem $QualityRootFolder -Filter "*64"
+            $Colo = Get-ChildItem $DestURL -Filter "*64"
             break
         }
         {$_ -ge 65 -and $_ -le 96} {
-            $Colo = Get-ChildItem $QualityRootFolder -Filter "*96"
+            $Colo = Get-ChildItem $DestURL -Filter "*96"
             break
         }
         {$_ -ge 97 -and $_ -le 128} {
-            $Colo = Get-ChildItem $QualityRootFolder -Filter "*128"
+            $Colo = Get-ChildItem $DestURL -Filter "*128"
             break
         }
         {$_ -ge 129 -and $_ -le 160} {
-            $Colo = Get-ChildItem $QualityRootFolder -Filter "*160"
+            $Colo = Get-ChildItem $DestURL -Filter "*160"
             break
         }
         {$_ -ge 161 -and $_ -le 192} {
-            $Colo = Get-ChildItem $QualityRootFolder -Filter "*192"
+            $Colo = Get-ChildItem $DestURL -Filter "*192"
             break
         }
         default {
@@ -73,15 +78,15 @@ function moveFiles {
         }
     }
 
-    $ColoRootFolder = "$QualityRootFolder\$Colo"
+    $DestURL = "$DestURL\$Colo"
 
     try {
-        $UnitFolder = Get-ChildItem -Path $ColoRootFolder -Filter "*-$UnitNumber*" | Where-Object { $_.Name -match "\-$UnitNumber[L|R]$" }
+        $UnitFolder = Get-ChildItem -Path $DestURL -Filter "*-$UnitNumber*" | Where-Object { $_.Name -match "\-$UnitNumber[L|R]$" }
         if ($null -eq $UnitFolder) {
             throw "No unit folder found matching the pattern *-$UnitNumber*"
         }
 
-        $FunctionalityRootFolder = "$ColoRootFolder\$UnitFolder"
+        $FunctionalityRootFolder = "$DestURL\$UnitFolder"
         Write-Host "Functionality Root Folder: $FunctionalityRootFolder"
 
         $FunctionalityFolder = Get-ChildItem -Path $FunctionalityRootFolder -Filter "*functionality*"
@@ -144,51 +149,61 @@ function search{
     $JobNumber = $JobNumberInput.text
     $Prefix =  $JobNumber.ToString().substring(0, 2) + "00"
 
-    $RootFolder = "$Base/$Prefix"
+    $DestURL = "$Base/$Prefix"
 
     try {
-        $JobFolders = Get-ChildItem -Path $RootFolder -Filter "*$JobNumber*"
-        if ($JobFolders.Count -eq 0) {
+        $JobNumberFolder = Get-ChildItem -Path $DestURL -Filter "*$JobNumber*"
+        if ($JobNumberFolder.Count -eq 0) {
             throw "No job folders found matching the pattern *$JobNumber*"
+        }elseif($JobNumberFolder.Count -gt 1){
+            #multiple selections found. get user input
+            $i = 0
+            Write-Host "=== SELECTION ===" -ForegroundColor Cyan
+            for ($i; $i -le $JobNumberFolder.Count - 1; $i++) {
+                $var = $JobNumberFolder[$i]
+                Write-Host "$i. $var"   
+            }
+            Write-Host "e. Exit"
+    
+            $choice = Read-Host "Please select an option"
+            $temp = ""
+            switch ($choice) {
+            "$_" {
+                $temp = $JobNumberFolder[$_]
+                $DestURL = "$DestUrl\$temp"
+            }
+            "e"{
+                Write-Host "Exiting..."
+                break;
+            }
+            Default { 
+                Write-Host "Invalid option, try again." -ForegroundColor Red; Start-Sleep -Seconds 1 }
+            }
         }
     } catch {
         Write-Host "Error: $_"
         return
     }
 
-    foreach ($job in $JobFolders) {
-        try {
-            $qualityPath = "$RootFolder\$job\2 Quality"
+    $temp = Get-ChildItem -Path $DestURL -Filter "*Quality*"
+    $DestURL = "$DestURl\$temp"
+    $Colo = Get-ChildItem $DestURL -Filter "*Colo $ColoNumber*"
 
-            Write-Host "Checking path: $qualityPath"
-
-            if (Test-Path $qualityPath) {
-                if (Get-ChildItem -Path $qualityPath -Filter "*Units*") {
-                    $QualityRootFolder = $qualityPath
-                    Write-Host "Quality Root Folder found: $QualityRootFolder"
-                } else {
-                    Write-Host "No Units folder found in: $qualityPath"
-                }
-            } else {
-                Write-Host "Path does not exist: $qualityPath"
-            }
-        } catch {
-            Write-Host "Error accessing path: $_"
-        }
-    }
-    $Colo = Get-ChildItem $QualityRootFolder -Filter "*Colo $ColoNumber*"
-
-    $ColoRootFolder = "$QualityRootFolder\$Colo"
+    $ColoRootFolder = "$DestUrl\$Colo"
     Write-Host $ColoRootFolder
     $UnitList = Get-ChildItem $ColoRootFolder
     $count = 0
     $missing = 0
     foreach($child in $UnitList){
-        if(Test-Path "$ColoRootFolder\$child\Functionality & calibration sheets\*.pdf"){
+        $TestPath = "$ColoRootFolder\$child\Functionality & calibration sheets\*.pdf"
+        if(Test-Path $TestPath){
             $count = $count + 1
             $Unit = Get-ChildItem -Path "$ColoRootFolder\$child\Functionality & calibration sheets" -Name
             Write-Host $Unit
-        }else {$missing = $missing + 1}
+        }else {
+            $missing = $missing + 1
+            Write-Host "Missing: $TestPath"
+        }
         } Write-Host "Units in Colo: $count"
         Write-Host "Missing $missing files"
         return 
@@ -237,8 +252,8 @@ $SearchFilesButton.Add_Click({
 })
 
 $Form = New-Object Windows.Forms.Form
-$Form.Text = "SwiftTransfer"
-$Form.Width = 250
+$Form.Text = "Ballard SwiftTransfer"
+$Form.Width = 300
 $Form.Height = 150
 
 $Form.Controls.add($JobNumberLabel)
